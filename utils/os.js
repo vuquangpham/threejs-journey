3c0ae47a81e1bb8f0d1e719f97e4f6f02194ca5a
vuquangpham <vuquangpham2909@gmail.com>
2023-08-15 15:59:58 +0700

    fix: bug when clone file (missing file)

--- utils/os.js
+++ utils/os.js
@@ -34,10 +34,10 @@ const createDirectory = (path) => {
 const copyFile = (source = '', destination = '') => {
     return new Promise((resolve, reject) => {
         // destination already exist => not run
-        if(fs.existsSync(destination)) reject(new Error('The destination has already existed yet'));
+        if(fs.existsSync(destination)) resolve('The destination has already existed yet');
 
         // source doesn't exist
-        if(!fs.existsSync(source)) reject(new Error('The source doesn\'t exist'));
+        if(!fs.existsSync(source)) resolve('The source doesn\'t exist');
 
         // clone file
         fs.copyFile(source, destination, (err) => {
