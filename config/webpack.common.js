710ca58fc0fb6ccd718452f4986862e5dd630cb2
vuquangpham <vuquangpham2909@gmail.com>
2023-10-08 11:40:10 +0700

    chore(webpack): remove empty line

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -25,7 +25,6 @@ module.exports = {
 
     module: {
         rules: [
-
             {
                 test: /\.(png|jpg|gif|jpe?g|svg|woff2?|fnt|webp|mp4|ogg|mp3|wav|bin|glb|gltf|glsl|md)$/,
                 type: 'asset/resource',
