8055c7ed29dad87339bf128bf4a94934a53ee869
vuquangpham <vuquangpham2909@gmail.com>
2024-01-19 18:18:51 +0700

    feat: simple shader

--- app/pages/shaders/shader-pattern/fragment.glsl
+++ app/pages/shaders/shader-pattern/fragment.glsl
@@ -9,12 +9,15 @@ void main() {
     //    gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
     //    gl_FragColor = vec4(vUv.yy, vUv.y, 1.0);
 
-    float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
-    strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    //    float strength = step(0.4, mod(vUv.x * 10.0 , 1.0));
+    //    strength *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+    //
+    //    float strengthY = step(0.4, mod(vUv.y * 10.0, 1.0));
+    //    strengthY *= step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
+    //
+    //    gl_FragColor = vec4(vec3(strength + strengthY), 1.0);
 
-    // step, reach limit => return 1, else return 0
-    //    strength = step(0.8, strength);
+    float strength = abs(vUv.x - 0.5);
 
-    //    gl_FragColor = vec4(vec3(strength), 1.0);
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
