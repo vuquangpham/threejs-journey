4afe72aed6d9fa5a63c33b207f52bf3a0338e0a2
vuquangpham <vuquangpham2909@gmail.com>
2024-01-19 20:46:58 +0700

    fix: add process.cwd()

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const {isPathExistAsync, createDirectory, copyFile} = require("./os");
+const path = require("path");
+const { isPathExistAsync, createDirectory, copyFile } = require("./os");
 
 /**
  * String to slug
@@ -7,15 +7,16 @@ const {isPathExistAsync, createDirectory, copyFile} = require("./os");
  * @return {string}
  * */
 const stringToSlug = (string) => {
-    if(!string) return '';
-    return string.normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')
-        .replace(/đ/g, 'd').replace(/Đ/g, 'D')
-        .replace(/[^\w ]+/g, '')
-        .replace(/ +/g, '-')
-        .toLowerCase();
+  if (!string) return "";
+  return string
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/Đ/g, "D")
+    .replace(/[^\w ]+/g, "")
+    .replace(/ +/g, "-")
+    .toLowerCase();
 };
-
 
 /**
  * Create JS Base with multiple files automatically
@@ -25,41 +26,51 @@ const stringToSlug = (string) => {
  * @return {void}
  * */
 const createJsBase = (appPath, baseName, pageId) => {
-    // check if folder exist or not
-    const baseDirectory = path.join(appPath, baseName);
+  // check if folder exist or not
+  const baseDirectory = path.join(appPath, baseName);
 
-    isPathExistAsync(baseDirectory)
-        .catch(async() => {
-            // create directory
-            createDirectory(baseDirectory);
-            const defaultTemplateDirectory = path.join(baseDirectory, '..', 'template');
+  isPathExistAsync(baseDirectory)
+    .catch(async () => {
+      // create directory
+      createDirectory(baseDirectory);
+      const defaultTemplateDirectory = path.join(
+        baseDirectory,
+        "..",
+        "template"
+      );
 
-            // create index.js file
-            copyFile(path.join(defaultTemplateDirectory, 'index.js'), path.join(baseDirectory, 'index.js'))
-                .then(result => {
-                    if(result === true) return;
-                    console.log(result);
-                });
+      // create index.js file
+      copyFile(
+        path.join(defaultTemplateDirectory, "index.js"),
+        path.join(baseDirectory, "index.js")
+      ).then((result) => {
+        if (result === true) return;
+        console.log(result);
+      });
 
-            // create template.js file
-            const result = await copyFile(path.join(defaultTemplateDirectory, 'template.js'), path.join(baseDirectory, 'template.js'));
-            if(result === true) return;
-            console.log(result);
-        })
-        .finally(() => {
-            // create pageId folder
-            const pageIdDirectory = path.join(baseDirectory, pageId);
-            createDirectory(pageIdDirectory);
+      // create template.js file
+      const result = await copyFile(
+        path.join(defaultTemplateDirectory, "template.js"),
+        path.join(baseDirectory, "template.js")
+      );
+      if (result === true) return;
+      console.log(result);
+    })
+    .finally(() => {
+      // create pageId folder
+      const pageIdDirectory = path.join(baseDirectory, pageId);
+      createDirectory(pageIdDirectory);
 
-            // create pageId file
-            copyFile(path.join(baseDirectory, 'template.js'), path.join(pageIdDirectory, 'index.js'))
-                .then(result => {
-                    if(result === true) return;
-                    console.log(result);
-                });
-        });
+      // create pageId file
+      copyFile(
+        path.join(baseDirectory, "template.js"),
+        path.join(pageIdDirectory, "index.js")
+      ).then((result) => {
+        if (result === true) return;
+        console.log(result);
+      });
+    });
 };
-
 
 /**
  * Create View Engine Base with multiple files automatically
@@ -69,56 +80,64 @@ const createJsBase = (appPath, baseName, pageId) => {
  * @return {void}
  * */
 const createEngineBase = (appPath, baseName, pageId) => {
-    // check if folder exist or not
-    const baseDirectory = path.join(appPath, baseName);
+  // check if folder exist or not
+  const baseDirectory = path.join(appPath, baseName);
 
-    isPathExistAsync(baseDirectory)
-        .catch(async() => {
-            // create directory first
-            createDirectory(baseDirectory);
+  isPathExistAsync(baseDirectory)
+    .catch(async () => {
+      // create directory first
+      createDirectory(baseDirectory);
 
-            // template path
-            const defaultTemplateDirectory = path.join(baseDirectory, '..', 'template');
-            const result = await copyFile(path.join(defaultTemplateDirectory, 'template.ejs'), path.join(baseDirectory, 'template.ejs'));
-            if(result === true) return;
-            console.log(result);
-
-        })
-        .finally(() => {
-            // create pageId file
-            copyFile(path.join(baseDirectory, 'template.ejs'), path.join(baseDirectory, pageId + '.ejs'))
-                .then(result => {
-                    if(result === true) return;
-                    console.log(result);
-                });
-        });
+      // template path
+      const defaultTemplateDirectory = path.join(
+        baseDirectory,
+        "..",
+        "template"
+      );
+      const result = await copyFile(
+        path.join(defaultTemplateDirectory, "template.ejs"),
+        path.join(baseDirectory, "template.ejs")
+      );
+      if (result === true) return;
+      console.log(result);
+    })
+    .finally(() => {
+      // create pageId file
+      copyFile(
+        path.join(baseDirectory, "template.ejs"),
+        path.join(baseDirectory, pageId + ".ejs")
+      ).then((result) => {
+        if (result === true) return;
+        console.log(result);
+      });
+    });
 };
-
 
 /**
  * Create pages prototype
  * */
-const createPagesPrototype = (prototypes = []) => prototypes.map(prototype => {
-    const {pages} = prototype;
+const createPagesPrototype = (prototypes = []) =>
+  prototypes.map((prototype) => {
+    const { pages } = prototype;
 
-    const appPagesPath = path.join('app', 'pages');
-    const viewEnginePath = path.join('views');
+    const appPagesPath = path.join(process.cwd(), "app", "pages");
+    const viewEnginePath = path.join(process.cwd(), "views");
 
     pages.forEach((page) => {
-        if(!page.id){
-            page.id = stringToSlug(page.title);
-        }
+      if (!page.id) {
+        page.id = stringToSlug(page.title);
+      }
 
-        // create js directory
-        createJsBase(appPagesPath, prototype.base, page.id);
+      // create js directory
+      createJsBase(appPagesPath, prototype.base, page.id);
 
-        // view engine template
-        createEngineBase(viewEnginePath, prototype.base, page.id);
+      // view engine template
+      createEngineBase(viewEnginePath, prototype.base, page.id);
     });
 
     return prototype;
-});
+  });
 
 module.exports = {
-    createPagesPrototype
+  createPagesPrototype,
 };
