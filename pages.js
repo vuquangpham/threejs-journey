ea533f2144f02f169940b84bfd475f66093a5dc7
vuquangpham <vuquangpham2909@gmail.com>
2024-01-31 21:04:26 +0700

    feat: introduce raging sea

--- pages.js
+++ pages.js
@@ -1,81 +1,84 @@
-const {createPagesPrototype} = require("./utils/utils");
+const { createPagesPrototype } = require("./utils/utils");
 module.exports = createPagesPrototype([
-    {
-        title: 'Basics',
-        base: 'basic',
-        pages: [
-            {
-                title: 'Three Basic'
-            },
-            {
-                title: 'Geometries'
-            },
-            {
-                title: 'Texture'
-            },
-            {
-                title: 'Material'
-            },
-            {
-                title: '3D Text'
-            }
-        ]
-    },
-    {
-        title: 'Classic Techniques',
-        base: 'technique',
-        pages: [
-            {
-                title: 'Shadows'
-            },
-            {
-                title: 'Haunted House'
-            },
-            {
-                title: 'Particles'
-            },
-            {
-                title: 'Galaxy Generator'
-            },
-            {
-                title: 'Scroll Based Animation'
-            }
-        ]
-    },
-    {
-        title: 'Advanced Techniques',
-        base: 'advanced',
-        pages: [
-            {
-                title: 'Physics'
-            },
-            {
-                title: 'Imported Models'
-            },
-            {
-                title: 'Raycaster and Mouse events'
-            },
-            {
-                title: 'Hamburger with Blender'
-            },
-            {
-                title: 'Environment Map'
-            },
-            {
-                title: 'Realistic Render'
-            }
-        ]
-    },
-    {
-        title: 'Shaders',
-        base: 'shaders',
-        pages: [
-            {
-                title: 'Shaders'
-            },
-            {
-                title: 'Shader Pattern'
-            }
-        ]
-    },
+  {
+    title: "Basics",
+    base: "basic",
+    pages: [
+      {
+        title: "Three Basic",
+      },
+      {
+        title: "Geometries",
+      },
+      {
+        title: "Texture",
+      },
+      {
+        title: "Material",
+      },
+      {
+        title: "3D Text",
+      },
+    ],
+  },
+  {
+    title: "Classic Techniques",
+    base: "technique",
+    pages: [
+      {
+        title: "Shadows",
+      },
+      {
+        title: "Haunted House",
+      },
+      {
+        title: "Particles",
+      },
+      {
+        title: "Galaxy Generator",
+      },
+      {
+        title: "Scroll Based Animation",
+      },
+    ],
+  },
+  {
+    title: "Advanced Techniques",
+    base: "advanced",
+    pages: [
+      {
+        title: "Physics",
+      },
+      {
+        title: "Imported Models",
+      },
+      {
+        title: "Raycaster and Mouse events",
+      },
+      {
+        title: "Hamburger with Blender",
+      },
+      {
+        title: "Environment Map",
+      },
+      {
+        title: "Realistic Render",
+      },
+    ],
+  },
+  {
+    title: "Shaders",
+    base: "shaders",
+    pages: [
+      {
+        title: "Shaders",
+      },
+      {
+        title: "Shader Pattern",
+      },
+      {
+        title: "Raging Sea",
+      },
+    ],
+  },
 ]);
