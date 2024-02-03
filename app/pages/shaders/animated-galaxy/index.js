012fdd33eeb276673eb4bedebec19d06b7470302
vuquangpham <vuquangpham2909@gmail.com>
2024-02-03 23:56:27 +0700

    feat: simple galaxy

--- app/pages/shaders/animated-galaxy/index.js
+++ app/pages/shaders/animated-galaxy/index.js
@@ -1,83 +1,250 @@
-import * as THREE from 'three';
-import {OrbitControls} from "three/addons/controls/OrbitControls";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls";
+import GUI from "lil-gui";
 
-export default class{
-    constructor({element}){
-        this.element = element;
-        this.init();
-    }
+import fragmentShader from "./fragment.glsl";
+import vertexShader from "./vertex.glsl";
 
-    init(){
-        // canvas
-        const canvas = document.querySelector('canvas#webgl');
+export default class {
+  constructor({ element }) {
+    this.element = element;
+    this.init();
+  }
 
-        // scene
-        const scene = new THREE.Scene();
+  init() {
+    /**
+     * Base
+     */
+    // Debug
+    const gui = new GUI();
 
-        /**
-         * Objects
-         */
-            // Material
-        const material = new THREE.MeshStandardMaterial();
-        material.roughness = 0.4;
+    // Canvas
+    const canvas = document.querySelector("canvas#webgl");
 
-        // Sizes
-        const sizes = {
-            width: window.innerWidth,
-            height: window.innerHeight
-        };
+    // Scene
+    const scene = new THREE.Scene();
 
-        // camera
-        const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-        camera.position.z = 2;
-        scene.add(camera);
+    /**
+     * Galaxy
+     */
+    const parameters = {};
+    parameters.count = 200000;
+    parameters.size = 0.005;
+    parameters.radius = 5;
+    parameters.branches = 3;
+    parameters.spin = 1;
+    parameters.randomness = 0.5;
+    parameters.randomnessPower = 3;
+    parameters.insideColor = "#ff6030";
+    parameters.outsideColor = "#1b3984";
 
-        // controls
-        const controls = new OrbitControls(camera, canvas);
-        controls.enableDamping = true;
+    let geometry = null;
+    let material = null;
+    let points = null;
 
-        // render
-        const renderer = new THREE.WebGLRenderer({
-            canvas: canvas
-        });
-        renderer.setSize(sizes.width, sizes.height);
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-        renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+    const generateGalaxy = () => {
+      if (points !== null) {
+        geometry.dispose();
+        material.dispose();
+        scene.remove(points);
+      }
 
-        const clock = new THREE.Clock();
+      /**
+       * Geometry
+       */
+      geometry = new THREE.BufferGeometry();
 
-        // update the frame
-        const render = () => {
-            const elapsedTime = clock.getElapsedTime();
+      const positions = new Float32Array(parameters.count * 3);
+      const colors = new Float32Array(parameters.count * 3);
+      const scales = new Float32Array(parameters.count * 1);
 
-            // update the controls
-            controls.update();
+      const insideColor = new THREE.Color(parameters.insideColor);
+      const outsideColor = new THREE.Color(parameters.outsideColor);
 
-            // render
-            renderer.render(scene, camera);
+      for (let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3;
 
-            // call on the next frame
-            window.requestAnimationFrame(render);
-        };
-        render();
+        // Position
+        const radius = Math.random() * parameters.radius;
 
-        // handle resize
-        window.addEventListener('resize', () => {
-            sizes.width = window.innerWidth;
-            sizes.height = window.innerHeight;
+        const branchAngle =
+          ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
 
-            // update the camera
-            camera.aspect = sizes.width / sizes.height;
-            camera.updateProjectionMatrix();
+        const randomX =
+          Math.pow(Math.random(), parameters.randomnessPower) *
+          (Math.random() < 0.5 ? 1 : -1) *
+          parameters.randomness *
+          radius;
+        const randomY =
+          Math.pow(Math.random(), parameters.randomnessPower) *
+          (Math.random() < 0.5 ? 1 : -1) *
+          parameters.randomness *
+          radius;
+        const randomZ =
+          Math.pow(Math.random(), parameters.randomnessPower) *
+          (Math.random() < 0.5 ? 1 : -1) *
+          parameters.randomness *
+          radius;
 
-            // update the render
-            renderer.setSize(sizes.width, sizes.height);
-            renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-        });
-    }
+        positions[i3] = Math.cos(branchAngle) * radius + randomX;
+        positions[i3 + 1] = randomY;
+        positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ;
 
-    // for destroy this script when navigating between each page
-    destroy(){
-        console.log('destroyed', this);
-    }
+        // Color
+        const mixedColor = insideColor.clone();
+        mixedColor.lerp(outsideColor, radius / parameters.radius);
+
+        colors[i3] = mixedColor.r;
+        colors[i3 + 1] = mixedColor.g;
+        colors[i3 + 2] = mixedColor.b;
+
+        scales[i] = Math.random();
+      }
+
+      geometry.setAttribute(
+        "position",
+        new THREE.BufferAttribute(positions, 3)
+      );
+      geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+      geometry.setAttribute("scale", new THREE.BufferAttribute(scales, 1));
+
+      /**
+       * Material
+       */
+      material = new THREE.ShaderMaterial({
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true,
+
+        fragmentShader,
+        vertexShader,
+
+        uniforms: {
+          uSize: { value: 2 },
+        },
+      });
+
+      /**
+       * Points
+       */
+      points = new THREE.Points(geometry, material);
+      scene.add(points);
+    };
+
+    generateGalaxy();
+
+    gui
+      .add(parameters, "count")
+      .min(100)
+      .max(1000000)
+      .step(100)
+      .onFinishChange(generateGalaxy);
+    gui
+      .add(parameters, "radius")
+      .min(0.01)
+      .max(20)
+      .step(0.01)
+      .onFinishChange(generateGalaxy);
+    gui
+      .add(parameters, "branches")
+      .min(2)
+      .max(20)
+      .step(1)
+      .onFinishChange(generateGalaxy);
+    gui
+      .add(parameters, "randomness")
+      .min(0)
+      .max(2)
+      .step(0.001)
+      .onFinishChange(generateGalaxy);
+    gui
+      .add(parameters, "randomnessPower")
+      .min(1)
+      .max(10)
+      .step(0.001)
+      .onFinishChange(generateGalaxy);
+    gui.addColor(parameters, "insideColor").onFinishChange(generateGalaxy);
+    gui.addColor(parameters, "outsideColor").onFinishChange(generateGalaxy);
+
+    gui
+      .add(material.uniforms.uSize, "value")
+      .min(0)
+      .max(10)
+      .step(1)
+      .name("pointSize");
+    /**
+     * Sizes
+     */
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    window.addEventListener("resize", () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+
+      // Update camera
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
+
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    /**
+     * Camera
+     */
+    // Base camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      0.1,
+      100
+    );
+    camera.position.x = 3;
+    camera.position.y = 3;
+    camera.position.z = 3;
+    scene.add(camera);
+
+    // Controls
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+
+    /**
+     * Renderer
+     */
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    /**
+     * Animate
+     */
+    const clock = new THREE.Clock();
+
+    const tick = () => {
+      const elapsedTime = clock.getElapsedTime();
+
+      // Update controls
+      controls.update();
+
+      // Render
+      renderer.render(scene, camera);
+
+      // Call tick again on the next frame
+      window.requestAnimationFrame(tick);
+    };
+
+    tick();
+  }
+
+  // for destroy this script when navigating between each page
+  destroy() {
+    console.log("destroyed", this);
+  }
 }
